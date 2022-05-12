dc73d20ecdcc9238295ffcac3347e978eee4a4b7
Xun Yang <xun.yang@modernaforsakringar.se>
2022-05-12 23:41:20 +0200

    Test out MovieDetail

--- components/MoviesList/MoviesList.tsx
+++ components/MoviesList/MoviesList.tsx
@@ -1,19 +1,38 @@
-import React from "react";
+import React, {useState} from "react";
 
-import {Movie} from '../../interfaces/Movie';
+import {MovieSearch} from '../../interfaces/MovieSearch';
+import axios from "axios";
+import {MovieDetail} from "../../interfaces/MovieDetail";
+import {MovieContent} from "../MovieContent/MovieContent";
 
-interface MovieItemProps extends Movie {
+interface MovieItemProps extends MovieSearch {
 
 }
 
 interface MoviesListProps {
-  movies: Movie[]
+  movies: MovieSearch[]
 }
 
-const MovieItem:React.FC<MovieItemProps> = ({Title, Year}) => {
+const MovieItem:React.FC<MovieItemProps> = ({Title, Year, imdbID}) => {
+  const [movieDetail, setMovieDetail] = useState<MovieDetail>({} as MovieDetail);
+  const onLinkClick = () => {
+    axios.get('/api/getMovie', {
+      params: {
+        id: imdbID
+      }
+    })
+      .then(response => setMovieDetail(response.data))//setMovies(response.data.Search));
+  }
   return <li>
-    <span>Title: {Title}</span>
-    <span>Release Year: {Year}</span>
+    <a onClick={onLinkClick}>
+      <span>Title: {Title}</span>
+      <span>Release Year: {Year}</span>
+    </a>
+
+    {
+      movieDetail &&
+      <MovieContent {...movieDetail}/>
+    }
   </li>
 }
 export const MoviesList:React.FC<MoviesListProps> = ({movies}) => {
