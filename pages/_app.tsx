1389b72716c147337a4df1bf0466b3fdf5ecb836
Xun Yang <xun.yang@modernaforsakringar.se>
2022-05-13 22:48:07 +0200

    Remove SearchContext

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,15 +2,12 @@ import {QueryClient, QueryClientProvider} from "react-query";
 import type { AppProps } from 'next/app'
 
 import '../styles/globals.css'
-import {SearchContextProvider} from "../contexts/SearchContext/SearchContextProvider";
 
 const queryClient = new QueryClient()
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <QueryClientProvider client={queryClient}>
-    <SearchContextProvider>
       <Component {...pageProps} />
-    </SearchContextProvider>
   </QueryClientProvider>
 }
 
